5af00d0ee03028b44d790c7ffe5f390b7a2b43f7
Joana Palero <joana.palero24@gmail.com>
2018-06-08 03:40:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,22 +2,8 @@ def my_collect(empty_array)
   i = 0
 array = []
   while i < empty_array.length
-    yield(array[i])
+   array << yield(array[i])
     i += 1
   end
-end
-
-array = []
-my_collect(empty_array) do |x|
-  name.split(" ").first
-end
-
-def my_collect(collection)
-  i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-  i += 1
-  end
-  new_collection
+  array
 end
